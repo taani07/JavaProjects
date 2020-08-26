d818c04c17c0a0b5059d5fa6ae7e18c192d49210
Tanishka <tanishkacs4061@gmail.com>
2020-08-25 21:35:35 -0700

    Stage the other changes

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -2,6 +2,7 @@ package emailapp;
 
 public class Email {
     public static void main(String[] args) {
+            EmailApp ea = new EmailApp("Tanishka","Chaturvedi");
 
     }
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class EmailApp {
     private String firstName;
     private String lastName;
@@ -7,7 +9,18 @@ public class EmailApp {
     private String department;
     private int mailboxCapacity;
     private String alternateEmailaddress;
+    //Constructor to recieve First Name and Last Name
+    public EmailApp(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created for :" +this.firstName +"."+ this.lastName);
 
+        //Method for asking department name
+
+        this.department =  setDepartment();
+
+    }
     public String getPassword() {
         return password;
     }
@@ -28,8 +41,16 @@ public class EmailApp {
         this.password = password;
     }
 
-    public void setDepartment(String department) {
-        this.department = department;
+    public String setDepartment() {
+        System.out.println("Enter the  department Details /n 1  /n 2 /n 3");
+        Scanner sc = new Scanner(System.in);
+        int deptChoice = sc.nextInt();
+        if(deptChoice == 0) { return "there is no such department";}
+        else if (deptChoice ==1 ){ return "Sales";}
+        else if (deptChoice == 2){return "Development" ;}
+        else if (deptChoice == 3){return "Accounting";}
+
+       return this.department = department;
     }
 
     public void setMailboxCapacity(int mailboxCapacity) {
